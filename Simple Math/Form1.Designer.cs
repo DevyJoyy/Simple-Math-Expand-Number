6e4f23035b22000a9c81eeb23d29932d69acecd9
Student <Student@DEV-JOY19>
2024-04-06 13:41:34 +0200

    Add project files.

---
+++ Simple Math/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Simple_Math
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.instLabel = new System.Windows.Forms.Label();
+            this.processButton = new System.Windows.Forms.Button();
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.outputTextbox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.closeButton.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.closeButton.Location = new System.Drawing.Point(1102, 23);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(82, 42);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "X";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.closeButton.MouseEnter += new System.EventHandler(this.closeButton_MouseEnter);
+            this.closeButton.MouseLeave += new System.EventHandler(this.closeButton_MouseLeave);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(12, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(248, 32);
+            this.title.TabIndex = 1;
+            this.title.Text = "Expand The Number";
+            // 
+            // instLabel
+            // 
+            this.instLabel.AutoSize = true;
+            this.instLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.instLabel.Location = new System.Drawing.Point(364, 132);
+            this.instLabel.Name = "instLabel";
+            this.instLabel.Size = new System.Drawing.Size(396, 28);
+            this.instLabel.TabIndex = 2;
+            this.instLabel.Text = "Enter The Number You Want To Expand :";
+            // 
+            // processButton
+            // 
+            this.processButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.processButton.Image = global::Simple_Math.Properties.Resources.settingsIcon;
+            this.processButton.Location = new System.Drawing.Point(519, 274);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(89, 55);
+            this.processButton.TabIndex = 5;
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextbox.Location = new System.Drawing.Point(356, 184);
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(415, 30);
+            this.inputTextbox.TabIndex = 6;
+            this.inputTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.outputTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputTextbox.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextbox.ForeColor = System.Drawing.Color.White;
+            this.outputTextbox.Location = new System.Drawing.Point(108, 425);
+            this.outputTextbox.Multiline = true;
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.ReadOnly = true;
+            this.outputTextbox.Size = new System.Drawing.Size(984, 69);
+            this.outputTextbox.TabIndex = 7;
+            this.outputTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // clearButton
+            // 
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clearButton.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(806, 184);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 30);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(1210, 596);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.outputTextbox);
+            this.Controls.Add(this.inputTextbox);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.instLabel);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.closeButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple math";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label instLabel;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.TextBox outputTextbox;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Simple Math/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Simple_Math
+{
+    public partial class Form1 : Form
+    {
+        int inputNumber;
+
+        public Point mouseLocation;
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+         (
+              int nLeftRect,
+              int nTopRect,
+              int nRightRect,
+              int nBottomRect,
+              int nWidthEllipse,
+             int nHeightEllipse
+
+          );
+
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void closeButton_MouseEnter(object sender, EventArgs e)
+        {
+            closeButton.ForeColor = Color.White;
+            closeButton.BackColor = Color.Red;
+        }
+
+        private void closeButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.BackColor = Color.FromArgb(45, 45, 48);
+            closeButton.ForeColor = Color.FromArgb(105, 105, 105);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mCurrentPos = Control.MousePosition;
+                mCurrentPos.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mCurrentPos;
+            }
+        }
+
+        void ExpandNum(string number)
+        {
+            string expandedNot = "";
+            string finalNotation = "";
+            int numLength = number.Length - 1;
+            for (int j = 0; j <= number.Length; j++)
+            {
+                if (j < number.Length - 1)
+                {
+                    if (number[j] != '0')
+                    {
+                        expandedNot = addZero(number[j], numLength);
+                        finalNotation += $"{expandedNot} + ";
+                        numLength--;
+                    }
+                    else
+                    {
+                        numLength--;
+                        continue;
+                    }
+                }
+                else if (j == number.Length - 1)
+                {
+                    if (number[j] != '0')
+                    {
+                        finalNotation += $"{number[number.Length - 1]}";
+                    }
+                    else
+                    {
+                        finalNotation = finalNotation.Remove(finalNotation.LastIndexOf('+') - 1);
+                        continue;
+                    }
+                }
+
+            }
+
+            outputTextbox.Text = finalNotation;
+        }
+
+        string addZero(Char character, int length)
+        {
+            string numWithZeros = Char.ToString(character);
+            for (int i = 1; i <= length; i++)
+            {
+                numWithZeros += "0";
+            }
+
+            return numWithZeros;
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            if (inputTextbox.Text != "")
+            {
+                if (int.TryParse(inputTextbox.Text, out inputNumber))
+                {
+                    ExpandNum(inputTextbox.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Enter Only Numeric Values");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter The Number(s) Please");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            inputTextbox.Clear();
+            outputTextbox.Clear();
+        }
+    }
+}
